582129e5818e46c7251c09b5e6bb078fa8e52285
Yasmeen mahilang <yasmeen2002mahilang@gmail.com>
2024-08-02 14:47:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import './styles/GlobalStyles.css';
+import { store } from './app/store';
+
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
